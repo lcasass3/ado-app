2e85226c57ffa32aff94d0ad9e485feac9b86f2e
Leon Casas <leonn.casass@gmail.com>
2024-07-18 11:04:49 -0600

    - Se inicia le ruta de register, su controlador con el metodo show para renderizar y el grupo de routas 'auth' en routes.ts

---
+++ app/controllers/auth/register_controller.ts
@@ -0,0 +1,7 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class RegisterController {
+  async show({ view }: HttpContext) {
+    return view.render('pages/auth/register')
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,14 @@
 |
 */
 
+const RegisterController = () => import('#controllers/auth/register_controller')
 import router from '@adonisjs/core/services/router'
+
+router
+  .group(() => {
+    router.get('/register', [RegisterController, 'show']).as('register')
+  })
+  .prefix('/auth')
+  .as('auth')
 
 router.on('/').render('pages/home')
