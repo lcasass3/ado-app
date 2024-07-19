68ddb25dd1176bbc2f4688d6c82777f8ff06fea4
Leon Casas <leonn.casass@gmail.com>
2024-07-19 10:38:02 -0600

    - Created register page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./resources/**/*.{edge,js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: '#2b2b2b',
+        background: '#f0f0f0',
+        primary: {
+          default: '#1d4f8b',
+          dark: '#163c69',
+        },
+        secondary: {
+          default: '#f7c6a1',
+          dark: '#f5b88a',
+        },
+        accent: {
+          default: '#ff6e61',
+          dark: '#ff4433',
+        },
+      },
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
   },
   plugins: [],
 }
