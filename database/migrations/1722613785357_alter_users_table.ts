33210c787498730c850b4a91f3e13d543ce08275
Leon Casas <leonn.casass@gmail.com>
2024-08-02 11:14:15 -0600

    - Deleted default content

--- database/migrations/1722613785357_alter_users_table.ts
+++ database/migrations/1722613785357_alter_users_table.ts
@@ -9,7 +9,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
       table.boolean('is_email_verified').defaultTo(false).notNullable()
-      table.string('email_verification_token').nullable().defaultTo(null).unique()
+      table.string('email_verification_token').nullable().unique()
     })
   }
 
